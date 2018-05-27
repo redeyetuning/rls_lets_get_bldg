859934bbcb9955662e4f0fd3d0757d0f7a76a951
Samuel Masters <redeyetuning@gmail.com>
2018-05-27 10:43:16 +0400

    Add User resource to routes and User#show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def index
   end
+
+  def create
+  end
+
+  def new
+  	
+  end
+
+  def edit
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
+  
 end
