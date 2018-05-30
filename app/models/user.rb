14e27aeb7008faf34f3b11dc9d5d9148c979504f
Samuel Masters <redeyetuning@gmail.com>
2018-05-30 10:30:17 +0400

    Add SessionsHelper logout and User forget

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,17 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:rember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
