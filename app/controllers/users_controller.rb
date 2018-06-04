ef927aa207ed38349db9f8fd5cdb49dd224b51c7
Samuel Masters <redeyetuning@gmail.com>
2018-06-04 13:02:41 +0400

    Add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only:[:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
 
   def index
@@ -24,7 +25,6 @@ class UsersController < ApplicationController
   
 
   def edit
-    @user = User.find_by(params[:id])
   end
 
   def show
@@ -32,7 +32,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find_by(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated!"
       redirect_to @user
@@ -53,8 +52,15 @@ private
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in!"
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,20 +21,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	end
   end
 
-  test "sucessful edit" do
-  	log_in_as(@user)
-  	get edit_user_path(@user)
-  	assert_template 'users/edit'
-  	name = "Foo Bar"
-  	email = "foo@bar.com"
-  	patch user_path(@user), params: { user: { name:  name,
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
                                               email: email,
                                               password:              "",
                                               password_confirmation: "" } }
-  	assert_not flash.empty?
-  	assert_redirected_to @user
-  	@user.reload
-  	assert_equal name, @user.name
-  	assert_equal email, @user.email
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
   end
 end
