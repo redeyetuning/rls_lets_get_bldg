1d4b46c9ddbff0f6dad5822d759a6c2ca18d9d9d
Samuel Masters <redeyetuning@gmail.com>
2018-06-05 12:48:57 +0400

    Add admin users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,6 +25,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              'foobar',
+                                            password_confirmation: 'foobar',
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -40,4 +50,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+	test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 end
